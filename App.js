d371fd142c62b63346c9d04b96cdb1a62fd2c914
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2024-12-29 17:07:36 +0000

    Update App.js

--- App.js
+++ App.js
@@ -4,11 +4,19 @@ import Greet from './componenents/Greet'
 import Welcome from './componenents/Welcome'
 import Hello from './componenents/Hello'
 import Message from './componenents/Message'
+import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <EventBind />
+      {/*<ClassClick />*/}
+      {/*<FunctionClick />*/}
+      {/*<Counter />*/}
+      {// <Message />}
       {/*<Greet name="Bruce" heroName="Batman">
         <p>This childrens props</p>
       </Greet>*/}
@@ -16,7 +24,6 @@ function App() {
         <button>Action</button>
       </Greet> */}
       {/* <Greet name="Diana" heroName="Wonder Women"></Greet> */}
-
       {/* <Welcome name="Bruce" heroName="Batman" /> */}
       {/*<Welcome name="Clark" heroName="Superman" /> */}
       {/*<Welcome name="Diana" heroName="Wonder Women" /> */}
